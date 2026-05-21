76a0477d800d6976f2240961a06d88a1e914cf5b
Minakshi <minakshi419singh@gmail.com>
2026-05-21 11:19:32 +0530

    exp

---
+++ c6.js
@@ -0,0 +1,20 @@
+// console.log("hello this is minakshi");
+// console.log(window);
+// console.dir(document);
+// console.dir(document.body);
+ //console.dir(document.body.childNodes[1]); 
+
+ // to change text of the html code
+//document.body.childNodes[3].innerText = "abcd";
+
+// document.body.style.background="green"; {to change color in console}
+
+//  let heading1= document.getElementById("head");
+//  console.dir(heading);
+//  let head= document.getElementById("head");
+//  console.dir(head);
+
+// for class similar to array
+let classes = document.getElementsByClassName("class1");
+console.dir(classes);
+console.log(classes);
